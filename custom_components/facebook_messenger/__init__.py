ca70fb945acdac3449e66949070172ad01e208cc
hoducnguyenhd <115439680+hoducnguyenhd@users.noreply.github.com>
2025-06-18 22:27:45 +0700

    Update __init__.py

--- custom_components/facebook_messenger/__init__.py
+++ custom_components/facebook_messenger/__init__.py
@@ -1,24 +1,20 @@
+"""Init file for Facebook Messenger integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
+
 from .const import DOMAIN
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     return True
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "notify")
-    )
     return True
+
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    await hass.config_entries.async_forward_entry_unload(entry, "notify")
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
-
-async def async_get_options_flow(config_entry):
-    from .options_flow import FacebookOptionsFlowHandler
-    return FacebookOptionsFlowHandler(config_entry)
