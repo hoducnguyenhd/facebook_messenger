4946ddf125faeb4f78d57c6f4f1fcaf695cd65e7
hoducnguyenhd <115439680+hoducnguyenhd@users.noreply.github.com>
2025-06-18 22:35:46 +0700

    Update config_flow.py

--- custom_components/facebook_messenger/config_flow.py
+++ custom_components/facebook_messenger/config_flow.py
@@ -1,25 +1,35 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from .const import DOMAIN, CONF_PAGE_ACCESS_TOKEN, CONF_TARGETS, CONF_SID, CONF_NAME
+from .const import DOMAIN, CONF_PAGE_ACCESS_TOKEN, CONF_TARGETS
 
 
 class FacebookMessengerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
+
         if user_input is not None:
-            return self.async_create_entry(title="Facebook Messenger", data=user_input)
+            return self.async_create_entry(
+                title="Facebook Messenger",
+                data={
+                    CONF_PAGE_ACCESS_TOKEN: user_input[CONF_PAGE_ACCESS_TOKEN],
+                    CONF_TARGETS: [x.strip() for x in user_input.get(CONF_TARGETS, "").split(",") if x.strip()]
+                }
+            )
 
-        schema = vol.Schema({
-            vol.Required(CONF_PAGE_ACCESS_TOKEN): str,
-            vol.Optional(CONF_TARGETS, default=[]): list
-        })
-        return self.async_show_form(step_id="user", data_schema=schema)
-
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_PAGE_ACCESS_TOKEN): str,
+                vol.Optional(CONF_TARGETS, default=""): str  # comma-separated list
+            }),
+            errors=errors
+        )
 
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
-        from .options_flow import FacebookMessengerOptionsFlowHandler
-        return FacebookMessengerOptionsFlowHandler(config_entry)
+        from .options_flow import FacebookMessengerOptionsFlow
+        return FacebookMessengerOptionsFlow(config_entry)
