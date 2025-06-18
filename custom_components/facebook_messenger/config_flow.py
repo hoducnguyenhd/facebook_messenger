e980940ad30a9ff0509e50b8d28eba614f715a4d
hoducnguyenhd <115439680+hoducnguyenhd@users.noreply.github.com>
2025-06-18 22:28:33 +0700

    Update config_flow.py

--- custom_components/facebook_messenger/config_flow.py
+++ custom_components/facebook_messenger/config_flow.py
@@ -1,43 +1,25 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.data_entry_flow import FlowResult
-from .const import DOMAIN, CONF_PAGE_ACCESS_TOKEN, CONF_TARGETS, CONF_NAME, CONF_SID
+from homeassistant.core import callback
+from .const import DOMAIN, CONF_PAGE_ACCESS_TOKEN, CONF_TARGETS, CONF_SID, CONF_NAME
 
-class FacebookConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+
+class FacebookMessengerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        errors = {}
         if user_input is not None:
             return self.async_create_entry(title="Facebook Messenger", data=user_input)
 
         schema = vol.Schema({
             vol.Required(CONF_PAGE_ACCESS_TOKEN): str,
-            vol.Optional(CONF_TARGETS): vol.All([{
-                vol.Required(CONF_NAME): str,
-                vol.Required(CONF_SID): str
-            }])
+            vol.Optional(CONF_TARGETS, default=[]): list
         })
-        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+        return self.async_show_form(step_id="user", data_schema=schema)
 
-from homeassistant import config_entries
-from .const import DOMAIN
 
-class FacebookOptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        data = self.config_entry.data
-        schema = vol.Schema({
-            vol.Required("page_access_token", default=data.get("page_access_token")): str,
-            vol.Optional("targets", default=data.get("targets", [])): vol.All([{
-                vol.Required("name"): str,
-                vol.Required("sid"): str
-            }])
-        })
-
-        return self.async_show_form(step_id="init", data_schema=schema)
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        from .options_flow import FacebookMessengerOptionsFlowHandler
+        return FacebookMessengerOptionsFlowHandler(config_entry)
