e87b1b1fcb0e977f92a3635da5329551aa2ff6f3
hoducnguyenhd <115439680+hoducnguyenhd@users.noreply.github.com>
2025-06-19 09:42:36 +0700

    Update __init__.py

--- custom_components/facebook_messenger/__init__.py
+++ custom_components/facebook_messenger/__init__.py
@@ -6,8 +6,12 @@ async def async_setup(hass, config):
 async def async_setup_entry(hass, entry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "notify")
+    )
     return True
 
 async def async_unload_entry(hass, entry):
+    await hass.config_entries.async_forward_entry_unload(entry, "notify")
     hass.data[DOMAIN].pop(entry.entry_id, None)
     return True
