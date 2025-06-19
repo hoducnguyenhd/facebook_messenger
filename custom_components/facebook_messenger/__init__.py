78b307909d56d014a74705a1a9eb1563f274dade
hoducnguyenhd <115439680+hoducnguyenhd@users.noreply.github.com>
2025-06-19 08:58:54 +0700

    Update __init__.py

--- custom_components/facebook_messenger/__init__.py
+++ custom_components/facebook_messenger/__init__.py
@@ -1,7 +1,13 @@
-from homeassistant import core
+DOMAIN = "facebook_messenger"
 
+async def async_setup(hass, config):
+    return True
 
-async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
-    """Set up the Facebook Messenger component."""
-    # @TODO: Add setup code.
+async def async_setup_entry(hass, entry):
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    return True
+
+async def async_unload_entry(hass, entry):
+    hass.data[DOMAIN].pop(entry.entry_id, None)
     return True
